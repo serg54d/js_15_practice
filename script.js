95d9750d7a58489ef955c876b7ef6dfa4e5e6946
Sergey <serg54d@bk.ru>
2021-06-16 17:57:53 +0300

    completed the third task

--- script.js
+++ script.js
@@ -41,8 +41,18 @@ for (let i = 0; i < 2; i++) {
         console.log('error');
         i--;
     }
-
-    
 }
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+
 
 console.log(personalMovieDB);
